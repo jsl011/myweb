fbc0cf082cc8164c0d16a9ba37033626e1379837
LiangJinSen <206291218@qq.com>
2022-11-01 19:18:55 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { message, Upload, Modal } from 'antd';
-import React, { useState } from 'react';
+import {LoadingOutlined, PlusOutlined} from '@ant-design/icons';
+import {message, Upload, Modal, Button} from 'antd';
+import React, {useState} from 'react';
 
 
 function App() {
@@ -17,6 +17,8 @@ function App() {
     const [previewImage, setPreviewImage] = useState('');
     const [previewTitle, setPreviewTitle] = useState('');
     const [fileList, setFileList] = useState([]);
+    const [base64, setBase64] = useState('')
+    let BASE64 = '';
     const handleCancel = () => setPreviewOpen(false);
     const handlePreview = async (file) => {
         if (!file.url && !file.preview) {
@@ -26,10 +28,19 @@ function App() {
         setPreviewOpen(true);
         setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
     };
-    const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+    const handleChange = ({fileList: newFileList}) => {
+        setFileList(newFileList)
+    };
+    const logBase64 = () => {
+        BASE64 = fileList[0]?.thumbUrl;
+        console.log("BASE64", BASE64)
+        console.log("base64", fileList[0]?.thumbUrl)
+        setBase64(fileList[0]?.thumbUrl)
+
+    };
     const uploadButton = (
         <div>
-            <PlusOutlined />
+            <PlusOutlined/>
             <div
                 style={{
                     marginTop: 8,
@@ -40,45 +51,48 @@ function App() {
         </div>
     );
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
 
-          <Upload
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              listType="picture-card"
-              fileList={fileList}
-              onPreview={handlePreview}
-              onChange={handleChange}
-          >
-              {fileList.length >= 8 ? null : uploadButton}
-          </Upload>
-          <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
-              <img
-                  alt="example"
-                  style={{
-                      width: '100%',
-                  }}
-                  src={previewImage}
-              />
-          </Modal>
+                <Upload
+                    // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    // onPreview={handlePreview}
+                    onChange={handleChange}
+                >
+                    {fileList.length >= 1 ? null : uploadButton}
+                </Upload>
+                <Button onClick={logBase64}>
+                    打印base64
+                </Button>
+                {/*<Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>*/}
+                {/*    <img*/}
+                {/*        alt="example"*/}
+                {/*        style={{*/}
+                {/*            width: '100%',*/}
+                {/*        }}*/}
+                {/*        src={previewImage}*/}
+                {/*    />*/}
+                {/*</Modal>*/}
 
-        {/*<p>*/}
-        {/*  Edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
-       </header>
-     </div>
+                {/*<p>*/}
+                {/*  Edit <code>src/App.js</code> and save to reload.*/}
+                {/*</p>*/}
+                {/*<a*/}
+                {/*  className="App-link"*/}
+                {/*  href="https://reactjs.org"*/}
+                {/*  target="_blank"*/}
+                {/*  rel="noopener noreferrer"*/}
+                {/*>*/}
+                {/*  Learn React*/}
+                {/*</a>*/}
+            </header>
+        </div>
 
-  );
+    );
 }
 
 export default App;
